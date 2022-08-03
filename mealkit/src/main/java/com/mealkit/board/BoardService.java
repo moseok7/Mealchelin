a62553e2a7f226bb81bf78839add42fc79b0871b
dnehftka@naver.com <dnehftka@naver.com>
2022-08-03 19:16:31 +0900

    프로젝트

---
+++ mealkit/src/main/java/com/mealkit/board/BoardService.java
@@ -0,0 +1,26 @@
+package com.mealkit.board;
+
+import java.util.List;
+
+public interface BoardService {
+
+  public void regist(BoardVO board) throws Exception;
+
+  public BoardVO read(Integer bno) throws Exception;
+
+  public void modify(BoardVO board) throws Exception;
+
+  public void remove(Integer bno) throws Exception;
+
+  public List<BoardVO> listAll() throws Exception;
+
+  public List<BoardVO> listCriteria(Criteria cri) throws Exception;
+
+  public int listCountCriteria(Criteria cri) throws Exception;
+
+  public List<BoardVO> listSearchCriteria(SearchCriteria cri) 
+      throws Exception;
+
+  public int listSearchCount(SearchCriteria cri) throws Exception;
+
+}
